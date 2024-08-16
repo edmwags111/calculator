19850750a2691a2cdf4785bc2dbe18c37759de9c
rayauxey <rayauxey@gmail.com>
2024-08-16 09:59:29 +0300

    Change title main

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Simple Calculator App',
+      title: 'Simple Calculator',
       theme: ThemeData(
         // This is the theme of your application.
         //
