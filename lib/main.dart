6714c25820b7cbf6120bbfa37f1e68a038d5587c
rayauxey <rayauxey@gmail.com>
2024-08-16 10:02:15 +0300

    faadda

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Simple Calculator',
+      title: 'Simple Calc',
       theme: ThemeData(
         // This is the theme of your application.
         //
